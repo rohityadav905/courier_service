1551b3e6b5058b2259be9d5a1d070fb214d02b3c
Rohit Yadav <rohityadav905@gmail.com>
2021-10-25 15:22:51 +0530

    Fix# Solve the query performance issues.

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -3,7 +3,7 @@ class ParcelsController < ApplicationController
 
   # GET /parcels or /parcels.json
   def index
-    @parcels = Parcel.all
+    @parcels = Parcel.all.includes(:sender, :service_type)
   end
 
   # GET /parcels/1 or /parcels/1.json
